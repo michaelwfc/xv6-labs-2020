eede705ae73c45f02a6d54b59607856cf67afa29
michaelwfc <wangfeicheng007@gmail.com>
2025-11-08 15:04:00 +0800

    finished lab4-traps-part1

--- user/call.c
+++ user/call.c
@@ -12,6 +12,14 @@ int f(int x) {
 }
 
 void main(void) {
+
   printf("%d %d\n", f(8)+1, 13);
+
+  printf("Q5\n");
+  unsigned int i = 0x00646c72;
+	printf("H%x Wo%s", 57616, &i);
+  printf("\nQ6\n");
+  printf("x=%d y=%d\n", 3);
+
   exit(0);
 }
